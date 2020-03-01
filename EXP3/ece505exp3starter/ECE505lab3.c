df90ab36aee0541b76b53d22b27236fc8d934fb1
dash2215 <59145612+dash2215@users.noreply.github.com>
2020-02-29 22:13:12 -0500

    Update ECE505lab3.c

--- EXP3/ece505exp3starter/ECE505lab3.c
+++ EXP3/ece505exp3starter/ECE505lab3.c
@@ -92,12 +92,11 @@ int main(void)
     }
 
     //
-    // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
+    // Enable the GPIO pin for the LED (PF1-3).  
+		//Set the direction as output, and
     // enable the GPIO pin for digital function.
-    //
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1);
-		//GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-		//GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+
 
 	
 		// Set up the serial console to use for displaying messages.  This is just
